f9a4591716655b9ca61b21fa9afd17214119d33a
0xBadawy <mohamedbadawysayed02@gmail.com>
2025-10-07 20:46:26 +0300

    Fix repository URL in ecosystem configuration for production deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
             user: "moment",
             host: "46.252.193.108",  
             ref: "origin/production",     
-            repo: "git clone ssh://git@github.com-badawy/Badawy403/weallbeam.git",
+            repo: "git@github.com:Badawy403/weallbeam.git",
             path: "/var/www/weallbeam/production-backend",
             "post-deploy": "npm install && pm2 reload ecosystem.config.js --env production",
             shared: ["uploads"]     
